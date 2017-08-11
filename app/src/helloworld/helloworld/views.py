7286379649d6ce45adbb5a56ea91843a2e2443f2
shubham-amura <shubham@amuratech.com>
2017-08-11 15:33:34 +0530

    Recording test

--- app/src/helloworld/helloworld/views.py
+++ app/src/helloworld/helloworld/views.py
@@ -9,6 +9,12 @@ def index(request):
     return HttpResponse("Hello Hasura World!")
 
 def recieve_call(request):
-    response = "<?xml version='1.0' encoding='UTF-8'?><Response><playtext>Thank you for calling Voura. We are always here to help you</playtext></Response>"
+    print(request)
+    response = "<?xml version='1.0' encoding='UTF-8'?>"+
+                "<Response>"+
+                  "<playtext speed='7' quality='best' >Thank you for calling Voura. We are always here to help you </playtext>"+
+                  "<playtext speed='6' quality='best'> Hey friend! Leave your question for us we will get back to you soon </playtext>"+
+                   "<record format='wav' silence='3' maxduration='40' >current_user</record>"
+                  "</Response>"
     return HttpResponse(response)
 
