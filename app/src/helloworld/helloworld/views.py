eeab69696207af05775049d65f32e82a84a3e170
shubham-amura <shubham@amuratech.com>
2017-08-11 15:37:00 +0530

    Format changed for recording

--- app/src/helloworld/helloworld/views.py
+++ app/src/helloworld/helloworld/views.py
@@ -10,11 +10,11 @@ def index(request):
 
 def recieve_call(request):
     print(request)
-    response = "<?xml version='1.0' encoding='UTF-8'?>"+
-                "<Response>"+
-                  "<playtext speed='7' quality='best' >Thank you for calling Voura. We are always here to help you </playtext>"+
-                  "<playtext speed='6' quality='best'> Hey friend! Leave your question for us we will get back to you soon </playtext>"+
-                   "<record format='wav' silence='3' maxduration='40' >current_user</record>"
+    response = "<?xml version='1.0' encoding='UTF-8'?>"\
+                "<Response>"\
+                  "<playtext speed='7' quality='best' >Thank you for calling Voura. We are always here to help you </playtext>"\
+                  "<playtext speed='6' quality='best'> Hey friend! Leave your question for us we will get back to you soon </playtext>"\
+                   "<record format='wav' silence='3' maxduration='40' >current_user</record>"\
                   "</Response>"
     return HttpResponse(response)
 
